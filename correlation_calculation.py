09e67585b1dce8aa8b0a598fce6d2ec3e49e0a48
Erika Dickson <143139501+erika-ld@users.noreply.github.com>
2024-09-21 14:47:50 -0700

    Updated the correlation file to pull data from Yahoo Finance using Yahoo Finance API. Currently have a data frame with stock data for TSLA

--- correlation_calculation.py
+++ correlation_calculation.py
@@ -1,10 +1,21 @@
 import numpy as np
 import pandas as pd
-import pandas_datareader as web
-from datetime import datetime
+import datetime as dt
 import matplotlib.pyplot as plt
 import seaborn
+import yfinance as yf
 
-#select start date for correlation window as well as list of tickers
-#start = datetime(2017, 1, 1)
+
+#Set start date variable as start date for historical data (9/20/23)
+start_date = dt.datetime(2023, 9, 20)
+#Set end date variable as end date for historical data (9/20/24)
+end_date = dt.datetime(2024, 9, 19)
+#Array of tickers for selected stocks: CHPT (Charge Point) & TSLA (Tesla)
 symbols_list = ['CHPT', 'TSLA']
+
+#Download data from Yahoo Finance using Yahoo Finance API, returns a data frame with stock data for that period 
+df = yf.download("TSLA", start=start_date, end=end_date)
+print(df)
+
+#An array to store adjusted closing prices
+symbols=[]
