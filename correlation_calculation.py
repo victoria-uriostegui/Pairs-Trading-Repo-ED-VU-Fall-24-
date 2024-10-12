de09a2c7139875f93376ecada01d171f0c53c9d4
Erika Dickson <143139501+erika-ld@users.noreply.github.com>
2024-10-12 15:49:30 -0700

    Added new file to move over statistical data to jupyter notebook + working on data preprocessing

--- correlation_calculation.py
+++ correlation_calculation.py
@@ -12,16 +12,6 @@ start_date = dt.datetime(2023, 9, 20)
 #Set end date variable as end date for historical data (9/20/24)
 end_date = dt.datetime(2024, 9, 19)
 
-#Download data from Yahoo Finance using Yahoo Finance API, returns a data frame with stock data for that period 
-tsla_df = yf.download("TSLA", start=start_date, end=end_date)
-tsla_df['Symbol'] = "TSLA"
-#print(tsla_df)
-
-
-chpt_df = yf.download("CHPT", start=start_date, end=end_date)
-chpt_df['Symbol'] = "CHPT"
-#print(chpt_df)
-
 
 #List of tickers for stocks to be compared (Auto Makers):
 symbols = ['TSLA', 'BMWKY', 'FUJHY', 'DRPRY', 'HMC', 'KS', 'MZDAY', 'TM', 'HYMTF']
@@ -30,25 +20,24 @@ symbols = ['TSLA', 'BMWKY', 'FUJHY', 'DRPRY', 'HMC', 'KS', 'MZDAY', 'TM', 'HYMTF
 symbols.extend(['CHPT', 'EVGO', 'BLNK', 'WBX', 'SPWRQ'])
 
 
-#Clean and transform the data to make it easier to view/utilize for correlation calculations
-
 #Dictionary variable to store the dataframes for each stock
 stocks = {}
 
+#Download data from Yahoo Finance using Yahoo Finance API, returns a data frame with stock data for that period 
 for symbol in symbols:
     stocks[symbol] = yf.download(symbol, start=start_date, end=end_date)
     stocks[symbol]['Symbol'] = symbol
 
+
+
+#Data preprocessing for all stock dataframes to make it easier to view/utilize for correlation calculations
 for key in stocks:
+    stocks[key].drop(columns = ['Open', 'High', 'Low', 'Close', 'Volume'], inplace = True)
+    print("NA:", stocks[key].isna().sum())
     print(key, ":", stocks[key].head(3), '\n\n')
     
 
-#tsla = tsla_df[['Close', 'Symbol']]
-
-#print(tsla)
-#chpt = chpt_df[[ 'Close', 'Symbol']]
-
-#stocks_df = pd.concat([tsla, chpt])
+#stocks_df = pd.concat([])
 #print(stocks_df)
 
 #stocks_df = stocks_df.pivot(columns = 'Symbol', values = 'Close')
